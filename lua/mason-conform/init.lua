6ac1ad8e3cfe7f066b22616bf102a6ecca163f5b
Pedro Gabriel de Morais Ribeiro <leo140201@hotmail.com>
2024-10-04 16:59:20 -0300

    Corrected default_handlers function

--- lua/mason-conform/init.lua
+++ lua/mason-conform/init.lua
@@ -1,41 +1,42 @@
 local M = {}
 
 function M.default_handlers(installed)
-	local registry = require("mason-registry")
-	local form = {}
-	for _, pkg_info in ipairs(registry.get_all_packages()) do
-		for _, i in ipairs(installed) do
-			if pkg_info.name == i then
-				for _, type in ipairs(pkg_info.spec.categories) do
-					if type == "Formatter" then
-						for _, lang in ipairs(pkg_info.spec.languages) do
-							lang = string.lower(lang)
-							if form[lang] == nil then
-								form[lang] = { pkg_info.name }
-							else
-								table.insert(form[lang], pkg_info.name)
-							end
-						end
-					end
-				end
-				break
-			end
-		end
-	end
-	return form
+    local registry = require("mason-registry")
+    local form = {}
+    for _, pkg_info in ipairs(registry.get_all_packages()) do
+        for _, i in ipairs(installed) do
+            if pkg_info.name == i then
+                for _, type in ipairs(pkg_info.spec.categories) do
+                    if type == "Formatter" then
+                        for _, lang in ipairs(pkg_info.spec.languages) do
+                            lang = string.lower(lang)
+                            if form[lang] == nil then
+                                form[lang] = { pkg_info.name }
+                            else
+                                table.insert(form[lang], pkg_info.name)
+                            end
+                        end
+                        break
+                    end
+                end
+                break
+            end
+        end
+    end
+    return form
 end
 
 M.opts = {
-	ensure_installed = {},
-	handlers = {},
+    ensure_installed = {},
+    handlers = {},
 }
 
 function M.setup(opts)
-	M.opts.ensure_installed = opts.ensure_installed or {}
-	M.opts.handlers = opts.handlers or {}
+    M.opts.ensure_installed = opts.ensure_installed or {}
+    M.opts.handlers = opts.handlers or {}
 
-	require("mason-conform.auto_install")()
-	require("conform").formatters_by_ft = M.opts.handlers
+    require("mason-conform.auto_install")()
+    require("conform").formatters_by_ft = M.opts.handlers
 end
 
 return M
