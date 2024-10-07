e419746235e8060a26374c09fe587c5a11af139c
Pedro Gabriel de Morais Ribeiro <leo140201@hotmail.com>
2024-10-06 22:42:56 -0300

    Updated plugin files

--- lua/mason-conform/auto_install.lua
+++ lua/mason-conform/auto_install.lua
@@ -2,25 +2,25 @@ local registry = require("mason-registry")
 local mapping = require("mason-conform.mapping")
 
 local function auto_install()
-    local config = require("mason-conform").opts
+	local config = require("mason-conform").opts
 
-    local formatters_to_install = {}
-    for _, formatter in pairs(config.ensure_installed) do
-        formatters_to_install[formatter] = 1
-    end
+	local formatters_to_install = {}
+	for _, formatter in pairs(config.ensure_installed) do
+		formatters_to_install[formatter] = true
+	end
 
-    for conformFormatter, _ in pairs(formatters_to_install) do
-        local package = mapping.conform_to_package[conformFormatter]
-        if package ~= nil then
-            require("mason-conform.install").try_install(package)
-        end
-    end
+	for conformFormatter, _ in pairs(formatters_to_install) do
+		local package = mapping.conform_to_package[conformFormatter]
+		if package ~= nil then
+			require("mason-conform.install").try_install(package)
+		end
+	end
 end
 
 if registry.refresh then
-    return function()
-        registry.refresh(vim.schedule_wrap(auto_install))
-    end
+	return function()
+		registry.refresh(vim.schedule_wrap(auto_install))
+	end
 else
-    return auto_install
+	return auto_install
 end
--- lua/mason-conform/init.lua
+++ lua/mason-conform/init.lua
@@ -1,37 +1,36 @@
 local M = {}
 
 M.opts = {
-    ensure_installed = {},
-    handlers = {},
+	ensure_installed = {},
+	handlers = {},
 }
 
 function M.default_handlers()
-    local registry = require("mason-registry")
-    local form = {}
-    for _, pkg_info in ipairs(registry.get_installed_packages()) do
-        for _, type in ipairs(pkg_info.spec.categories) do
-            if type == "Formatter" then
-                for _, lang in ipairs(pkg_info.spec.languages) do
-                    lang = string.lower(lang)
-                    if form[lang] == nil then
-                        form[lang] = { pkg_info.name }
-                    else
-                        table.insert(form[lang], pkg_info.name)
-                    end
-                end
-                break
-            end
-        end
-    end
-    return form
+	local registry = require("mason-registry")
+	local languages = {}
+	for _, pkg_info in ipairs(registry.get_installed_packages()) do
+		for _, type in ipairs(pkg_info.spec.categories) do
+			if type == "Formatter" then
+				for _, lang in ipairs(pkg_info.spec.languages) do
+					lang = string.lower(lang)
+					if not languages[lang] then
+						languages[lang] = {}
+					end
+					table.insert(languages[lang], pkg_info.name)
+				end
+				break
+			end
+		end
+	end
+	return languages
 end
 
 function M.setup(opts)
-    M.opts.ensure_installed = opts.ensure_installed or {}
-    M.opts.handlers = opts.handlers or {}
+	M.opts.ensure_installed = opts.ensure_installed or {}
+	M.opts.handlers = opts.handlers or {}
 
-    require("mason-conform.auto_install")()
-    require("conform").formatters_by_ft = M.opts.handlers
+	require("mason-conform.auto_install")()
+	require("conform").formatters_by_ft = M.opts.handlers
 end
 
 return M
